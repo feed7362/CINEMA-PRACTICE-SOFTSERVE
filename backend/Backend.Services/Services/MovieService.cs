dd6a2cbd4cd1f89b83e765e532e4a36261a4d5f7
Alex <bojkosasa78@gmail.com>
2026-01-17 15:55:48 +0100

    Simple example of using specifications

---
+++ backend/Backend.Services/Services/MovieService.cs
@@ -0,0 +1,28 @@
+﻿
+using Backend.Domain.Entities;
+using Backend.Domain.Interfaces;
+using static Backend.Services.Specifications.Movies;
+
+namespace Backend.Services.Services
+{
+    public class MovieService
+    {
+        private readonly IRepository<Movie> _movieRepository;
+
+        public MovieService(IRepository<Movie> movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public Movie GetMovie(string actorName)
+        {
+            var movie = _movieRepository.GetFirstBySpec(new ByActorName(actorName)); // Using the specification ByActorName from Backend.Services.Specifications.Movies to get the movie by actor name
+
+            if (movie == null) { 
+                throw new Exception("Movie not found");
+            }
+
+            return movie;
+        }
+    }
+}
