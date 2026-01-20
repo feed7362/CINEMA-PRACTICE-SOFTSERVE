4e40219685d530031db02d9cc8017c9ef135da74
Kostiantyn L <feed7362@gmail.com>
2026-01-20 13:40:58 +0200

    feat: configure UTF-8 encoding for console input and output

--- backend/Backend.API/Program.cs
+++ backend/Backend.API/Program.cs
@@ -1,5 +1,11 @@
 using Backend.API.Extensions;
 using Backend.Data;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
