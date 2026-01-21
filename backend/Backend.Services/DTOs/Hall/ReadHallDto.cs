a331a3a616d3d2b6e146766920ab40f5dd9c37dd
Kostiantyn L <feed7362@gmail.com>
2026-01-21 13:22:38 +0200

    feat: add DTOs for Hall and Session entities to support create and update operations

---
+++ backend/Backend.Services/DTOs/Hall/ReadHallDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Services.DTOs.Hall;
+
+public class ReadHallDto
+{
+    public int Id { get; set; }      
+    public string Name { get; set; }
+    public string Format { get; set; }
+    public int Capacity { get; set; }
+}
