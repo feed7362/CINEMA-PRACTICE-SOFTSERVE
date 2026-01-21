b4f80e0a4c61da117f6683ed7a447acf2883b13a
Alex <bojkosasa78@gmail.com>
2026-01-21 17:14:33 +0100

    Connecting all instances

--- backend/Backend.API/Program.cs
+++ backend/Backend.API/Program.cs
@@ -1,11 +1,16 @@
 using Backend.API.Extensions;
 using Backend.Data;
-using System.Text;
 using Backend.Data.Repositories;
 using Backend.Domain.Interfaces;
 using Backend.Services.Interfaces;
 using Backend.Services.Services;
+using FluentValidation;
 using Microsoft.OpenApi;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Backend.Services.Validators.Hall;
+
 
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 Console.OutputEncoding = Encoding.UTF8;
@@ -56,8 +61,35 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddValidatorsFromAssemblyContaining<CreateHallDtoValidator>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+app.UseCors("Default");
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
@@ -70,7 +102,7 @@ app.Lifetime.ApplicationStarted.Register(() => { Console.WriteLine("Application 
 app.Lifetime.ApplicationStopping.Register(() => { Console.WriteLine("Application stopping"); });
 app.SwaggerUi();
 
-
+app.MapAuthEndpoints();
 app.MapHallEndpoints();
 app.MapSessionEndpoints();
 
