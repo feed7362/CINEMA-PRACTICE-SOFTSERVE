e0ba077ae54f176f4739d8a10d2ef2100e90f58a
Alex <bojkosasa78@gmail.com>
2026-01-23 12:50:37 +0100

    booking validator

--- backend/Backend.API/Extensions/ValidationFilter.cs
+++ backend/Backend.API/Extensions/ValidationFilter.cs
@@ -4,21 +4,31 @@ namespace Backend.API.Extensions;
 
 public class ValidationFilter<T> : IEndpointFilter
 {
+    private readonly IValidator<T>? _validator;
+
+    public ValidationFilter(IValidator<T>? validator = null)
+    {
+        _validator = validator;
+    }
+
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
-        var validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
-        if (validator is not null)
+        if (_validator is not null)
         {
             var entity = context.Arguments.OfType<T>().FirstOrDefault();
-            if (entity is not null)
+
+            if (entity is null)
             {
-                var validationResult = await validator.ValidateAsync(entity);
-                if (!validationResult.IsValid)
-                {
-                    return Results.ValidationProblem(validationResult.ToDictionary());
-                }
+                return Results.BadRequest(new { error = $"The request body for {typeof(T).Name} is required." });
+            }
+
+            var validationResult = await _validator.ValidateAsync(entity);
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
             }
         }
+
         return await next(context);
     }
 }
---
+++ backend/Backend.Services/Validators/Booking/CreateBookingValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Backend.Services.DTOs.Booking;
+
+namespace Backend.Services.Validators;
+
+public class CreateBookingValidator : AbstractValidator<CreateBookingDto>
+{
+    public CreateBookingValidator()
+    {
+        RuleFor(x => x.SessionId)
+            .GreaterThan(0)
+            .WithMessage("Потрібно вказати дійсний ID сеансу.");
+
+        RuleFor(x => x.SeatIds)
+            .NotEmpty()
+            .WithMessage("Потрібно вибрати хоча б одне місце.")
+            .Must(list => list != null && list.All(id => id > 0))
+            .WithMessage("Усі ID місць повинні бути дійсними додатними числами.");
+
+        RuleFor(x => x.SeatIds)
+            .Must(list => list.Count <= 10)
+            .WithMessage("Неможливо забронювати більше 10 місць за одну транзакцію.");
+    }
+}
