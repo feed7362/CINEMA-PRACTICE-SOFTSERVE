18214409adb61af106d00c560dccfcdd0abff835
Alex <bojkosasa78@gmail.com>
2026-01-27 10:50:47 +0100

    service method for locking booking with tickets + exceptions

--- backend/Backend.Services/Interfaces/IBookingService.cs
+++ backend/Backend.Services/Interfaces/IBookingService.cs
@@ -1,10 +1,9 @@
-﻿
+﻿using Backend.Services.DTOs;
 using Backend.Services.DTOs.Booking;
-using Backend.Services.DTOs;
 
 public interface IBookingService
 {
-    Task<BookingResponseDto> CreateBookingAsync(CreateBookingDto dto, int userId);
+    Task<BookingResponseDto> LockBookingAsync(CreateBookingDto dto, int userId);
     Task<BookingResponseDto?> GetBookingByIdAsync(int bookingId, int userId);
     Task<BookingDetailDto?> GetBookingDetailsByIdAsync(int bookingId, int userId);
     Task<PagedResponse<BookingSummaryDto>> GetUserBookingHistoryAsync(int userId, int page = 1, int pageSize = 10);
