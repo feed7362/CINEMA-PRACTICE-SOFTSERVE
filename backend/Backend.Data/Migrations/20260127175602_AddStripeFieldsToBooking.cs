37a4fecc4b5239b63a69dd780158f70c74dc1b2b
Alex <bojkosasa78@gmail.com>
2026-01-28 10:05:50 +0100

    etitity change adding payment field

---
+++ backend/Backend.Data/Migrations/20260127175602_AddStripeFieldsToBooking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStripeFieldsToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConfirmationTime",
+                table: "Bookings",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Bookings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmationTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Bookings");
+        }
+    }
+}
