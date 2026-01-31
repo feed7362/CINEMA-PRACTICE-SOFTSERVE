c108fea67c4206f898100a9f4a7d1e0604060b98
Kostiantyn L <feed7362@gmail.com>
2026-01-31 15:01:35 +0200

    refactor: update IRepository and ISessionService interfaces, enhance ReadSessionDto immutability

--- backend/Backend.Domain/Interfaces/IRepository.cs
+++ backend/Backend.Domain/Interfaces/IRepository.cs
@@ -1,20 +1,21 @@
 ﻿using Ardalis.Specification;
 using Microsoft.EntityFrameworkCore.Storage;
 using System.Data;
+using System.Linq.Expressions;
 
 namespace Backend.Domain.Interfaces
 {
     public interface IRepository<TEntity> where TEntity : class, IEntity
     {
         Task<List<TEntity>> GetAllAsync();
-        Task<TEntity?> GetByIdAsync(int id);
+        Task<TEntity?> GetByIdAsync(int? id);
         void Insert(TEntity entity);
-        void Update(TEntity entity);
-        void Delete(int id);
-        void Delete(TEntity entity);
 
         IEnumerable<TEntity> GetListBySpec(ISpecification<TEntity> specification);
         TEntity? GetFirstBySpec(ISpecification<TEntity> specification);
+        
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+
 
         Task<TEntity> AddAsync(TEntity entity);
         Task UpdateAsync(TEntity entity);
