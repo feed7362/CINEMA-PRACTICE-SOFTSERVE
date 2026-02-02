4a55d6a76ff18d91547385f8cf9c19330808af00
Kostiantyn L <feed7362@gmail.com>
2026-02-02 21:54:34 +0200

    misc fixes + migrations

--- backend/Backend.API/Extensions/CorsExtensions.cs
+++ backend/Backend.API/Extensions/CorsExtensions.cs
@@ -8,7 +8,7 @@ public static class CorsExtensions
         {
             options.AddPolicy("Default", policy =>
             {
-                p.WithOrigins("http://localhost:5173")
+                policy.WithOrigins("http://localhost:5173")
                  .WithMethods("GET", "POST")
                  .WithHeaders("Content-Type", "Authorization");
             });
