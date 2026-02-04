8cd67141e00f0b2fa70257e86b0775862dd8d484
Alex <bojkosasa78@gmail.com>
2026-02-04 10:03:35 +0100

    google login endpoint

--- backend/Backend.API/Controllers/AuthEndpoints.cs
+++ backend/Backend.API/Controllers/AuthEndpoints.cs
@@ -4,7 +4,7 @@ using Backend.Services.DTOs.Auth;
 using Backend.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
-
+using Google.Apis.Auth;
 namespace Backend.API.Controllers;
 
 internal static class AuthEndpoints
@@ -59,6 +59,50 @@ internal static class AuthEndpoints
         })
             .AddEndpointFilter<ValidationFilter<RegisterDto>>()
             .WithName("Register");
+
+        group.MapPost("/external-login", async (
+            ExternalLoginDto dto,
+            UserManager<ApplicationUser> userManager,
+            ITokenService tokenService,
+            IConfiguration config) =>
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { config["Google:ClientId"]! }
+            };
+
+            var payload = await GoogleJsonWebSignature.ValidateAsync(dto.IdToken, settings);
+            if (payload == null) return Results.BadRequest("Invalid Google Token");
+
+            var user = await userManager.FindByEmailAsync(payload.Email);
+
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = payload.Email,
+                    Email = payload.Email,
+                    ProfilePictureUrl = payload.Picture,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user);
+                await userManager.AddToRoleAsync(user, "Customer");
+            }
+
+            var info = new UserLoginInfo("Google", payload.Subject, "Google");
+            var existingLogin = await userManager.GetLoginsAsync(user);
+
+            if (existingLogin.All(x => x.ProviderKey != info.ProviderKey))
+            {
+                await userManager.AddLoginAsync(user, info);
+            }
+
+            // standard JWT returned
+            var roles = await userManager.GetRolesAsync(user);
+            var token = tokenService.CreateToken(user, roles);
+
+            return Results.Ok(new { Token = token, Email = user.Email, Roles = roles });
+        });
 
         group.MapGet("/me", async (
                 ClaimsPrincipal claims,
