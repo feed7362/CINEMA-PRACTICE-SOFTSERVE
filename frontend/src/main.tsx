53002551d4d96e4cc1a95fcadf5ae1ad2b1334a7
Alex <bojkosasa78@gmail.com>
2026-02-04 10:06:59 +0100

    google provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
-import App from '@/App.tsx' 
+import App from '@/App.tsx'
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <GoogleOAuthProvider clientId={"кину в дс, не комітити"}>
+          <App />
+      </GoogleOAuthProvider>
   </StrictMode>,
 )
