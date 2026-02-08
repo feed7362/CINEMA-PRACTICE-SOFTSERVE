41a7476d87a8d178890ea6851a58c70d1b5ad927
Alex <bojkosasa78@gmail.com>
2026-02-08 18:16:23 +0100

    discount dtos

---
+++ backend/Backend.Services/DTOs/Discount/CreateDiscountDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Services.DTOs.Discount;
+public record CreateDiscountDto(
+        string Code, 
+        int Percentage, 
+        DateTime ExpiryDate
+    );
---
+++ backend/Backend.Services/DTOs/Discount/DiscountResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Services.DTOs.Discount;
+
+public record DiscountResponseDto(
+        int Id, 
+        string Code, 
+        int Percentage, 
+        DateTime ExpiryDate
+    );
