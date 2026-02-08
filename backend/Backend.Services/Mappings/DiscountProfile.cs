d8a736e3dcb113f66d1b414a1ef471a076320e74
Alex <bojkosasa78@gmail.com>
2026-02-08 18:16:58 +0100

    automapper for discount

---
+++ backend/Backend.Services/Mappings/DiscountProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Backend.Services.DTOs.Discount;
+using Backend.Domain.Entities;
+
+namespace Backend.Services.Mappings;
+
+public class DiscountProfile : Profile
+{
+    public DiscountProfile()
+    {
+        CreateMap<Discount, DiscountResponseDto>();
+        CreateMap<CreateDiscountDto, Discount>()
+            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
+    }
+}
